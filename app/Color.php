3f92ff560e7a6987be189a54a61f3e5443c65137
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-07 15:11:44 +0000

    color enum

---
+++ app/Color.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+enum Color: string
+{
+    case RED = "red";
+    case YELLOW = "yellow";
+    case GREEN = "green";
+    case BLUE = "blue";
+    case GRAY = "gray";
+    case PURPLE = "purple";
+}
