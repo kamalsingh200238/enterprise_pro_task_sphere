1dd9e6fdbdd8b2f2639cc77084f503378a128de1
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-07 19:57:53 +0000

    project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,9 +10,6 @@ class Project extends Model
 {
     use HasFactory, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     */
     protected $fillable = [
         'name',
         'description',
@@ -26,9 +23,6 @@ class Project extends Model
         'supervisor_id'
     ];
 
-    /**
-     * The attributes that should be cast.
-     */
     protected $casts = [
         'start_date' => 'datetime',
         'due_date' => 'datetime',
@@ -37,57 +31,36 @@ class Project extends Model
         'updated_at' => 'datetime'
     ];
 
-    /**
-     * Relationship: Project belongs to a status
-     */
     public function status()
     {
         return $this->belongsTo(Status::class);
     }
 
-    /**
-     * Relationship: Project belongs to a priority
-     */
     public function priority()
     {
         return $this->belongsTo(Priority::class);
     }
 
-    /**
-     * Relationship: User who created the project
-     */
-    public function creator()
+    public function createdBy()
     {
         return $this->belongsTo(User::class, 'created_by');
     }
 
-    /**
-     * Relationship: User who last updated the project
-     */
-    public function updater()
+    public function updatedBy()
     {
         return $this->belongsTo(User::class, 'updated_by');
     }
 
-    /**
-     * Relationship: Project supervisor
-     */
     public function supervisor()
     {
         return $this->belongsTo(User::class, 'supervisor_id');
     }
 
-    /**
-     * Relationship: Project assignees
-     */
     public function assignees()
     {
         return $this->belongsToMany(User::class, 'project_assignees');
     }
 
-    /**
-     * Relationship: Project viewers (for private projects)
-     */
     public function viewers()
     {
         return $this->belongsToMany(User::class, 'project_viewers');
