62a341bb5bdb2dd7d0062560839cabf853726020
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-09 15:28:33 +0000

    use enum color to get all the color values and make the name unique

--- database/migrations/2025_02_05_223107_create_priorities_table.php
+++ database/migrations/2025_02_05_223107_create_priorities_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Color;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +12,8 @@ return new class extends Migration {
     {
         Schema::create('priorities', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->enum('color', ['red', 'yellow', 'green', 'blue', 'gray', 'purple'])->default('gray');
+            $table->string("name")->unique();
+            $table->enum('color', array_column(Color::cases(), 'value'))->default(Color::GRAY->value);
             $table->timestampsTz();
         });
     }
