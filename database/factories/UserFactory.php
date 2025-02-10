a266ee1cd9a0d63246f499f33c75b6bd901fab1d
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-10 05:30:32 +0000

    user factory changes
    - function to change role of user
    - remove email verified at

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -26,19 +27,22 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function admin(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+        return $this->state(fn(array $attributes) => [
+            'role' => UserRole::ADMIN,
+        ]);
+    }
+
+    public function supervisor(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'role' => UserRole::SUPERVISOR,
         ]);
     }
 }
