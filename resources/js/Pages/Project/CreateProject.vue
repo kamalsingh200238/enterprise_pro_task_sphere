9da00751549f8a45fdebe5d70df9d73709c834b2
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-10 12:35:36 +0000

    temp changes

---
+++ resources/js/Pages/Project/CreateProject.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+console.log('this');
+</script>
+
+<template>HIHI</template>
--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,25 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/', function () {
+        return Inertia::render('Welcome');
+    });
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('/projects/new', function () {
+        return Inertia::render('Project/CreateProject');
+    });
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
