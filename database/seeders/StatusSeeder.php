e82e6e6befd8a0cd641928416f59c9af4d74b7f4
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-10 04:45:57 +0000

    seeder to seed basic status

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Status;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Color;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $statuses = [
+            [
+                'name' => 'Backlog',
+                'color' => Color::GRAY,
+            ],
+            [
+                'name' => 'In Progress',
+                'color' => Color::BLUE,
+            ],
+            [
+                'name' => 'On Hold',
+                'color' => Color::GRAY,
+            ],
+            [
+                'name' => 'In Review',
+                'color' => Color::GREEN,
+            ],
+            [
+                'name' => 'Done',
+                'color' => Color::GREEN,
+            ],
+        ];
+
+        foreach ($statuses as $status) {
+            Status::firstOrCreate(
+                ["name" => $status['name']],
+                $status
+            );
+        }
+    }
+}
