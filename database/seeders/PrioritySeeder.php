164431067b83b29e3ae2c2cb959d98f35b9e3936
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-10 04:45:48 +0000

    seeder to seed basic priorities

---
+++ database/seeders/PrioritySeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Color;
+use App\Models\Priority;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PrioritySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $priorities = [
+            [
+                'name' => 'Low',
+                'color' => Color::GRAY,
+            ],
+            [
+                'name' => 'Medium',
+                'color' => Color::BLUE,
+            ],
+            [
+                'name' => 'High',
+                'color' => Color::YELLOW,
+            ],
+            [
+                'name' => 'Urgent',
+                'color' => Color::RED,
+            ],
+        ];
+
+        foreach ($priorities as $priority) {
+            Priority::firstOrCreate(
+                ["name" => $priority['name']],
+                $priority
+            );
+        }
+    }
+}
