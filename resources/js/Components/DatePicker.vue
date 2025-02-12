4e29841cb13a6efefdc6631936a2fe406477adde
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-12 13:54:50 +0000

    date picker component

---
+++ resources/js/Components/DatePicker.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Button } from '@/Components/ui/button';
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/Components/ui/popover';
+import { Calendar } from '@/Components/ui/v-calendar';
+import { cn } from '@/lib/utils';
+import { format, formatISO, parseISO } from 'date-fns';
+import { Calendar as CalendarIcon } from 'lucide-vue-next';
+import { computed } from 'vue';
+
+const dateString = defineModel<string>();
+const date = computed({
+    get() {
+        if (!dateString.value) return undefined;
+        return parseISO(dateString.value);
+    },
+    set(newDate) {
+        dateString.value = newDate ? formatISO(newDate) : '';
+    },
+});
+const formatDate = (date: Date) => format(date, 'MMM d, yyyy');
+</script>
+<template>
+    <Popover>
+        <PopoverTrigger as-child>
+            <Button
+                variant="outline"
+                :class="
+                    cn(
+                        'w-[280px] justify-start text-left font-normal',
+                        !date && 'text-muted-foreground',
+                    )
+                "
+            >
+                <CalendarIcon class="mr-2 h-4 w-4" />
+                {{ date ? formatDate(date) : 'Pick a date' }}
+            </Button>
+        </PopoverTrigger>
+        <PopoverContent class="w-auto p-0">
+            <Calendar v-model="date" initial-focus />
+        </PopoverContent>
+    </Popover>
+</template>
