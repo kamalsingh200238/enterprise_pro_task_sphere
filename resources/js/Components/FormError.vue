0d67ff76163dc5bd98ed4ed02e956cea2ee6035b
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-12 10:11:41 +0000

    form error

---
+++ resources/js/Components/FormError.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+
+const props = defineProps({
+    err: String,
+    class: String,
+});
+</script>
+
+<template>
+    <div v-if="props.err" :class="cn('mt-1 text-sm text-red-500', props.class)">
+        {{ props.err }}
+    </div>
+</template>
