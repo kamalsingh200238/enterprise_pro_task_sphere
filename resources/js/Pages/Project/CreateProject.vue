a321f7a394e0baaf650e0a85b0030aa5578e8421
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-15 23:01:13 +0000

    use every component in create project form

--- resources/js/Pages/Project/CreateProject.vue
+++ resources/js/Pages/Project/CreateProject.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
+import AssigneeSelector from '@/Components/AssigneeSelector.vue';
 import DatePicker from '@/Components/DatePicker.vue';
 import FormError from '@/Components/FormError.vue';
+import PrioritySelect from '@/Components/PrioritySelect.vue';
+import StatusSelect from '@/Components/StatusSelect.vue';
+import SupervisorSelect from '@/Components/SupervisorSelect.vue';
 import { Button } from '@/Components/ui/button';
+import { Checkbox } from '@/Components/ui/checkbox';
 import { Input } from '@/Components/ui/input';
 import { Label } from '@/Components/ui/label';
 import { Textarea } from '@/Components/ui/textarea';
-import { useForm } from '@inertiajs/vue3';
-
-import StatusSelect from '@/Components/StatusSelect.vue';
-
-import PrioritySelect from '@/Components/PrioritySelect.vue';
+import ViewerSelector from '@/Components/ViewerSelector.vue';
 import { Priority, Status, User } from '@/types';
-import UserSelect from '@/Components/UserSelect.vue';
+import { Head, useForm } from '@inertiajs/vue3';
+import { watch } from 'vue';
 
 interface Props {
     statuses: Status[];
@@ -21,26 +23,28 @@ interface Props {
 
 const props = defineProps<Props>();
 
-const form = useForm<{
+type FormType = {
     name: string;
     description: string;
     start_date: string;
     due_date: string;
-    status_id: string;
-    priority_id: string;
+    status_id: number | null;
+    priority_id: number | null;
     is_private: boolean;
-    supervisor_id?: string;
+    supervisor_id: number | null;
     assignees: number[];
     viewers: number[];
-}>({
+};
+
+const form = useForm<FormType>({
     name: '',
     description: '',
     start_date: '',
     due_date: '',
-    status_id: '',
-    priority_id: '',
+    status_id: null,
+    priority_id: null,
     is_private: false,
-    supervisor_id: '',
+    supervisor_id: null,
     assignees: [],
     viewers: [],
 });
@@ -55,8 +59,9 @@ const submit = () => {
 </script>
 
 <template>
-    <div class="mx-auto max-w-2xl py-8">
-        <form @submit.prevent="submit" class="space-y-6">
+    <Head title="Create New Project" />
+    <main>
+        <form @submit.prevent="submit">
             <div>
                 <Label for="name">Project Name </Label>
                 <Input id="name" type="text" v-model="form.name" />
@@ -68,14 +73,18 @@ const submit = () => {
                 <FormError :err="form.errors.description" />
             </div>
             <div class="flex gap-5">
-                <div class="flex flex-col gap-1">
-                    <Label for="start-date">Start Date</Label>
-                    <DatePicker id="start-date" v-model="form.start_date" />
+                <div>
+                    <div class="flex flex-col gap-1">
+                        <Label for="start-date">Start Date</Label>
+                        <DatePicker id="start-date" v-model="form.start_date" />
+                    </div>
                     <FormError :err="form.errors.start_date" />
                 </div>
-                <div class="flex flex-col gap-1">
-                    <Label for="due-date">Due Date</Label>
-                    <DatePicker id="due-date" v-model="form.due_date" />
+                <div>
+                    <div class="flex flex-col gap-1">
+                        <Label for="due-date">Due Date</Label>
+                        <DatePicker id="due-date" v-model="form.due_date" />
+                    </div>
                     <FormError :err="form.errors.due_date" />
                 </div>
             </div>
@@ -85,7 +94,7 @@ const submit = () => {
                     id="status"
                     v-model="form.status_id"
                     :statuses="props.statuses"
-                ></StatusSelect>
+                />
                 <FormError :err="form.errors.status_id" />
             </div>
             <div>
@@ -94,15 +103,40 @@ const submit = () => {
                     id="priority"
                     v-model="form.priority_id"
                     :priorities="props.priorities"
-                ></PrioritySelect>
+                />
                 <FormError :err="form.errors.priority_id" />
             </div>
             <div>
-                <UserSelect :users="props.users" />
+                <AssigneeSelector
+                    :users="props.users"
+                    v-model="form.assignees"
+                />
+                <FormError :err="form.errors.assignees" />
+            </div>
+            <div>
+                <SupervisorSelect :users="users" v-model="form.supervisor_id" />
+                <FormError :err="form.errors.supervisor_id" />
+            </div>
+            <div>
+                <ViewerSelector
+                    :users="users"
+                    v-model="form.viewers"
+                    :assignee-ids="form.assignees"
+                />
+                <FormError :err="form.errors.supervisor_id" />
+            </div>
+            <div class="flex items-center space-x-2">
+                <Checkbox id="is-private" v-model="form.is_private" />
+                <label
+                    for="is-private"
+                    class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                    Private
+                </label>
             </div>
             <Button type="submit" :disabled="form.processing">
                 Create Project
             </Button>
         </form>
-    </div>
+    </main>
 </template>
