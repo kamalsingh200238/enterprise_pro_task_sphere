0f2943a22107cbd05e3da8523893b483c47b1a1d
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-18 15:35:53 +0000

    sub task model

---
+++ app/Models/SubTask.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SubTask extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'task_id',
+        'name',
+        'description',
+        'start_date',
+        'due_date',
+        'status_id',
+        'priority_id',
+        'is_private',
+        'created_by',
+        'updated_by',
+        'supervisor_id',
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'due_date' => 'datetime',
+        'is_private' => 'boolean',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($project) {
+            $project->slug = 'STASK-' . $project->id;
+            $project->saveQuietly();
+        });
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    public function priority()
+    {
+        return $this->belongsTo(Priority::class);
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function supervisor()
+    {
+        return $this->belongsTo(User::class, 'supervisor_id');
+    }
+
+    public function assignees()
+    {
+        return $this->belongsToMany(User::class, 'task_assignees');
+    }
+
+    public function viewers()
+    {
+        return $this->belongsToMany(User::class, 'task_viewers');
+    }
+}
