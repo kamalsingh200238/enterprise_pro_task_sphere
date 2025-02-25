8efe71113e31a73fba5bd18d67a003c3afca7067
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-25 22:15:18 +0000

    simple page to list of all projects

---
+++ resources/js/Pages/Project/AllProjects.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import BaseLayout from '@/Layouts/BaseLayout.vue';
+import { Project } from '@/types';
+import { Link } from '@inertiajs/vue3';
+
+interface Props {
+    projects: Project[];
+}
+
+function projectLink(id: number) {
+    return `/projects/${id}`;
+}
+
+const props = defineProps<Props>();
+</script>
+<template>
+    <BaseLayout> all projects page </BaseLayout>
+    <div v-for="project in projects" :key="project.id">
+        <Link :href="projectLink(project.id)">
+            <p>Project name: {{ project.name }}</p>
+            <p>Project slug: {{ project.slug }}</p>
+        </Link>
+    </div>
+</template>
