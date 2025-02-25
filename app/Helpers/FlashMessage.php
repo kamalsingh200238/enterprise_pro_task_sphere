0b14c28032dc774e0bdfd57065c148ace87d4b61
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-25 00:49:10 +0000

    flash message class to create a flash message

---
+++ app/Helpers/FlashMessage.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Helpers;
+
+class FlashMessage
+{
+    public string $heading;
+
+    public string $description;
+
+    public string $variant;
+
+    public int $duration;
+
+    public function __construct(string $heading, string $description = '', string $variant = 'success', int $duration = 5000)
+    {
+        $this->heading = $heading;
+        $this->description = $description;
+        $this->variant = $variant;
+        $this->duration = $duration;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'heading' => $this->heading,
+            'description' => $this->description,
+            'variant' => $this->variant,
+            'duration' => $this->duration,
+        ];
+    }
+}
