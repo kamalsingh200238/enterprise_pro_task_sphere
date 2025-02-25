f247b62c50f32e7c6ee3803c0e40c336814e006c
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-25 09:14:03 +0000

    create baselayout that will handle a lot of things

---
+++ resources/js/Layouts/BaseLayout.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { usePage } from '@inertiajs/vue3';
+import { computed, watch } from 'vue';
+import { toast } from 'vue-sonner';
+
+const page = usePage();
+const flash = computed(() => page.props.flash);
+watch(flash, (newValue) => {
+    if (!newValue) {
+        return;
+    }
+
+    switch (newValue.variant) {
+        case 'success':
+            toast.success(newValue.heading, {
+                description: newValue.description,
+                duration: newValue.duraton,
+                closeButton: true,
+            });
+            break;
+        case 'danger':
+            toast.error(newValue.heading, {
+                description: newValue.description,
+                duration: newValue.duraton,
+                closeButton: true,
+            });
+            break;
+    }
+});
+</script>
+
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
