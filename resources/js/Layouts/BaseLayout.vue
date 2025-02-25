80fc044bdf54fa94d5650d0f05d686412d8618aa
Kamal Singh <kamalsingh200238@gmail.com>
2025-02-25 22:13:21 +0000

    base layout with toast

--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -1,36 +1,64 @@
 <script setup lang="ts">
+import { Toaster } from '@/Components/ui/sonner';
 import { usePage } from '@inertiajs/vue3';
-import { computed, watch } from 'vue';
+import { computed, nextTick, watch } from 'vue';
 import { toast } from 'vue-sonner';
 
 const page = usePage();
 const flash = computed(() => page.props.flash);
-watch(flash, (newValue) => {
-    if (!newValue) {
-        return;
-    }
+const createdProject = computed(() => page.props.createdProject);
 
-    switch (newValue.variant) {
-        case 'success':
-            toast.success(newValue.heading, {
-                description: newValue.description,
-                duration: newValue.duraton,
+watch(
+    flash,
+    (newValue) => {
+        if (!newValue) {
+            return;
+        }
+
+        switch (newValue.variant) {
+            case 'success':
+                nextTick(() => {
+                    toast.success(newValue.heading, {
+                        description: newValue.description,
+                        duration: newValue.duraton,
+                        closeButton: true,
+                    });
+                });
+                break;
+            case 'danger':
+                nextTick(() => {
+                    toast.error(newValue.heading, {
+                        description: newValue.description,
+                        duration: newValue.duraton,
+                        closeButton: true,
+                    });
+                });
+                break;
+        }
+    },
+    { immediate: true },
+);
+
+watch(
+    createdProject,
+    (newValue) => {
+        if (!newValue) {
+            return;
+        }
+        nextTick(() => {
+            toast.success('Created project successfully', {
+                duration: 10000,
                 closeButton: true,
             });
-            break;
-        case 'danger':
-            toast.error(newValue.heading, {
-                description: newValue.description,
-                duration: newValue.duraton,
-                closeButton: true,
-            });
-            break;
-    }
-});
+        });
+    },
+    { immediate: true },
+);
 </script>
 
 <template>
     <div>
+        <Toaster rich-colors />
         <slot />
     </div>
 </template>
