6655f80a29f96aa9f4da7c6b9c231a9279e28c0f
Kamal Singh <kamalsingh200238@gmail.com>
2025-03-03 21:03:32 +0000

    make uuid primary

--- database/migrations/2025_03_02_220425_create_comments_table.php
+++ database/migrations/2025_03_02_220425_create_comments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->text('content');
             $table->foreignId('user_id')->constrained('users');
             $table->morphs('commentable');
