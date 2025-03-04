71885872f50b0eaf066b8b2adaa90a3fc00109dc
Kamal Singh <kamalsingh200238@gmail.com>
2025-03-04 12:13:08 +0000

    get intials of user

---
+++ resources/js/lib/getIntials.ts
@@ -0,0 +1,9 @@
+// Function to get initials from a user's name
+export const getInitials = (name: string) => {
+    return name
+        .split(' ')
+        .map((word) => word[0])
+        .join('')
+        .toUpperCase()
+        .slice(0, 2);
+};
