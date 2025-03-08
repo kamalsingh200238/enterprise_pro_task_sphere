aea3a268e9fb6780a335e75a581c216ea3cc8a42
MSH Fahim <168389138+SharafatFahim@users.noreply.github.com>
2025-03-08 11:42:11 +0000

    view to show all tasks

---
+++ software/resources/js/Pages/task/ShowAllTasks.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// import BaseLayout from '@/Layouts/BaseLayout.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Task } from '@/types';
+import { Link } from '@inertiajs/vue3';
+
+interface Props {
+    tasks: Task[];
+}
+
+function projectLink(id: number) {
+    return `/projects/${id}`;
+}
+
+defineProps<Props>();
+</script>
+<template>
+    <!-- <BaseLayout> all projects page </BaseLayout> -->
+    <AppLayout>
+        <div v-for="task in tasks" :key="task.id">
+            <Link :href="projectLink(task.id)">
+                <p>Project name: {{ task.name }}</p>
+                <p>Project slug: {{ task.slug }}</p>
+            </Link>
+        </div>
+    </AppLayout>
+</template>
