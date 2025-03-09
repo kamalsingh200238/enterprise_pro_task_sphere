cecc0287eb520b56d2282de0d284f207eccda1b0
smei123 <brie_bee05@outlook.com>
2025-03-09 17:01:43 +0000

    page to show all sub tasks

---
+++ software/resources/js/pages/sub-task/ShowAllSubTasks.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { SubTask } from '@/types';
+import { Link } from '@inertiajs/vue3';
+
+interface Props {
+    subTasks: SubTask[];
+}
+
+function subTaskLink(id: number) {
+    return `/sub-tasks/${id}`;
+}
+
+defineProps<Props>();
+</script>
+<template>
+    <AppLayout>
+        <div v-for="subTask in subTasks" :key="subTask.id">
+            <Link :href="subTaskLink(subTask.id)">
+                <p>Project name: {{ subTask.name }}</p>
+                <p>Project slug: {{ subTask.slug }}</p>
+            </Link>
+        </div>
+    </AppLayout>
+</template>
