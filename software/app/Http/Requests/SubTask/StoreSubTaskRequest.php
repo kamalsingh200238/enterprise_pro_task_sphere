d621508a21e7ef402d4a8eff1ae2db0eb9fbd339
smei123 <brie_bee05@outlook.com>
2025-03-09 16:42:50 +0000

    request class to validate incoming data for sub task

---
+++ software/app/Http/Requests/SubTask/StoreSubTaskRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\SubTask;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSubTaskRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'task_id' => ['required', 'exists:tasks,id'],
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'start_date' => ['required', 'date'],
+            'due_date' => ['required', 'date', 'after_or_equal:start_date'],
+            'status_id' => ['required', 'exists:statuses,id'],
+            'priority_id' => ['required', 'exists:priorities,id'],
+            'supervisor_id' => ['required', 'exists:users,id'],
+            'assignees' => ['required', 'array', 'min:1'],
+            'assignees.*' => ['exists:users,id'],
+            'is_private' => ['boolean'],
+            'viewers' => ['array', 'prohibited_unless:is_private,true'],
+            'viewers.*' => ['exists:users,id'],
+        ];
+    }
+}
