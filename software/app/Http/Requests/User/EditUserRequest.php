8cdbc51123465303dba3b48f2a5c669de9bf3f83
Kamal Singh <kamalsingh200238@gmail.com>
2025-03-10 17:20:34 +0000

    request class to validate data for edit user

---
+++ software/app/Http/Requests/User/EditUserRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Enums\UserRole;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class EditUserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($this->route('user'))],
+            'password' => ['nullable', 'min:8'],
+            'role' => ['required', Rule::enum(UserRole::class)]
+        ];
+    }
+}
