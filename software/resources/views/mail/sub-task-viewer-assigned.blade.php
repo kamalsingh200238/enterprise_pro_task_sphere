1c47400ba7a6e971aab2b022fbb6392d4c2c644d
Suhaana <hussainsuhaana@gmail.com>
2025-03-24 23:44:26 +0000

    email view for when a viewer is assigned to a sub task

---
+++ software/resources/views/mail/sub-task-viewer-assigned.blade.php
@@ -0,0 +1,19 @@
+{{-- WARN: do not indent this code, and do not change formatting of the code --}}
+<x-mail::message>
+# Sub-Task Viewer Assignment Notification
+
+Hello {{ $user->name }},
+
+You have been added as a viewer to a sub-task.
+
+Sub-Task name: **{{ $subTask->name }} ({{ $subTask->slug }})**
+
+Please click the button below to view the sub-task details.
+
+<x-mail::button :url="url('/sub-tasks/' . $task->id)">
+    View Sub-Task
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
