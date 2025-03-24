c1f45cd64e577105636578e2813ba2e1a2d23cd5
Daniel Amadike <amadykedaniel@gmail.com>
2025-03-24 23:00:18 +0000

    mail class to send email when a viewer is set for a task

---
+++ software/app/Mail/TaskViewerAssigned.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class TaskViewerAssigned extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Task $task;
+
+    public User $user;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Task $task, User $user)
+    {
+        $this->task = $task;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'You have been added as a viewer to a task',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.task-viewer-assigned',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
