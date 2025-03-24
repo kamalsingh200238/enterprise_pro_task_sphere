0273a6e00dea758236656ae3d33a6f44f934c0a2
Suhaana <hussainsuhaana@gmail.com>
2025-03-24 23:45:26 +0000

    mail class to send email when a viewer is set in sub task

---
+++ software/app/Mail/SubTaskViewerAssigned.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\SubTask;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SubTaskViewerAssigned extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public SubTask $subTask;
+
+    public User $user;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(SubTask $subTask, User $user)
+    {
+        $this->subTask = $subTask;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'You have been added as a viewer to a sub-task',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.sub-task-viewer-assigned',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
