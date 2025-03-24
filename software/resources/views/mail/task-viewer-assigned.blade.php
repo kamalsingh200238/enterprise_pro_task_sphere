d1586b28d0f6432f9537123a8d0bc382b0fdb76d
Daniel Amadike <amadykedaniel@gmail.com>
2025-03-24 22:59:24 +0000

    email for when a user is a viewer in a task

---
+++ software/resources/views/mail/task-viewer-assigned.blade.php
@@ -0,0 +1,19 @@
+{{-- WARN: do not indent this code, and do not change formatting of the code --}}
+<x-mail::message>
+# Task Viewer Assignment Notification
+
+Hello {{ $user->name }},
+
+You have been added as a viewer to a task.
+
+Task name: **{{ $task->name }} ({{ $task->slug }})**
+
+Please click the button below to view the task details.
+
+<x-mail::button :url="url('/task/' . $task->id)">
+    View Task
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
